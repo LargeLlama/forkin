9aafa5f66a776deded5a54e9bc77a18e69891933
Rubin Peci <rubinpeci@gmail.com>
2018-11-16 17:01:49 -0500

    get the code working for one child, need to work with 2 now

--- main.c
+++ main.c
@@ -1,44 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
-#include <time.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <time.h>
 
 int main()
 {
-	int time_sleep_0 = 0;
-	int time_sleep_1 = 0;
-	srand(time(NULL));
+	int time_slept_0 = 0;
+	int child_0;
+	srand( time(NULL) );
+	printf("What's cracking\n");
+	child_0 = fork();
+	wait(&time_slept_0);
 
-	int child_0 = fork();
-	int child_1 = fork();
-
-	if (child_0 > 0 && child_1 > 0)
+	if(!child_0)
 	{
-		printf("This is the parent process, pid: %d\n", getpid());
-		waitpid(child_0, &time_sleep_0, 0);
-		waitpid(child_1, &time_sleep_1, 0);
-
-		printf("child_0 was asleep for %d seconds\n", time_sleep_0);
-		printf("child_1 was asleep for %d seconds\n", time_sleep_1);
-		printf("DONE!\n");
-
-	} 
-	else if(!child_0 && child_1 > 0)
-	{
-		printf("This is the first child process, pid: %d\n", getpid());
-		time_sleep_0 = (rand() % 15) + 5;
-		sleep(time_sleep_0);
-		printf("CHILD 0 DONE!\n");
-		exit(time_sleep_0);
+		printf("hey losers, child 0 reporting for duty with a pid of %d\n", getpid());
+		time_slept_0 = (rand() % 16) + 5;
+		sleep(time_slept_0);
+		exit(time_slept_0);
 	}
-	else if(child_0 && !child_1)
-	{
-		printf("This is the second child process, pid: %d\n", getpid());
-		time_sleep_1 = (rand() % 15) + 5;
-		sleep(time_sleep_1);
-		printf("CHILD 1 DONE!\n");
-		exit(time_sleep_0);
-	}
+	printf("the pid of child 0 was %d\n", child_0);
+	printf("time slept for child 0 was %d seconds\n", time_slept_0 / 255 );
 }
